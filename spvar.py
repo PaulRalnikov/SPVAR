823f3f12a6943a864ac1d378fe01696663637143
DungeonMaster <st106332@student.spbu.ru>
2024-05-16 18:38:34 +0300

    Too many changes for only one commit name

--- spvar.py
+++ spvar.py
@@ -1,116 +1,155 @@
+import dimod.utilities
 import numpy as np
 import qubovert
 import dimod
-from math import sqrt
+from dataclasses import dataclass
+import scipy.stats as ss
 
 EPS = 0.001
 
-def generate_sample(Q, sample_size : int, anneal_duration) -> np.ndarray:
+def generate_sample(h : dict, J : dict, sample_size : int, anneal_duration : int = 1000) -> qubovert.sim.AnnealResults:
+    (Q, _) = dimod.ising_to_qubo(h, J)
     return qubovert.sim.anneal_qubo(Q, num_anneals=sample_size, anneal_duration=anneal_duration)
 
-def spvar(h,
-          J, 
-          sample_size : int,
-          fixing_threshold : float,
-          elite_threshold : float,
-          anneal_duration
-          ) -> np.ndarray:
-    
-    n = len(h.keys())
-    (Q, _) = dimod.ising_to_qubo(h, J)
+@dataclass
+class SPVAR_default_params:
+    h : dict
+    J : dict
+    sample_size : int
+    fixing_threshold : int
+    elite_threshold: int
+    anneal_duration: int = 1000
 
-    samples = generate_sample(Q, sample_size, anneal_duration)
-    samples.sort()
-    samples = samples[:int(sample_size * elite_threshold)]
+@dataclass
+class SPVAR_test_honest_params:
+    h : dict
+    J : dict
+    total_num_anneals: int
+    SPVAR_num_anneals : int
+    fixing_threshold : int
+    elite_threshold: int
 
-    new_sample_size = len(samples)
-
-    sum_appear = np.full(n, 0)
-    for i in range (n):
-        for sample in samples:
-            sum_appear[i] += 2 * sample.state[i] - 1
-
-    middle = [x / new_sample_size for x in np.nditer(sum_appear)]
-
-    deviation = np.full(n, 0)
-    for i in range (n):
-        for sample in samples:
-            val = (2 * sample.state[i] - 1 - middle[i])
-            deviation[i] += val * val
-
-    fix = np.array([(sqrt(x / new_sample_size) < fixing_threshold + EPS) for x in np.nditer(deviation)])
-    fixed = dict()
-
-    for i in range(n):
-        if (fix[i]):
-            fixed[i] = 1 if sum_appear[i] > 0 else -1
-
-    offset = 0
-
-    for i in range(n):
-        if (fix[i]):
-            if ((i, i) in J):
-                offset += J[(i, i)]
-                J.pop((i, i))
-        
-        for j in range(i + 1, n):
-            if not fix[i] and not fix[j]:
-                continue
-
-            sum_J = J.get((i, j), 0) + J.get((j, i), 0)
-
-            if fix[i] and fix[j]:
-                offset += sum_J * fixed[i] * fixed[j]
-                
-            if not fix[i]:
-                h[i] += sum_J * fixed[j]
-            if not fix[j]:
-                h[j] += sum_J * fixed[i]
-
-            if ((i, j) in J):
-                J.pop((i, j))
-            if (j, i) in J:
-                J.pop((j, i))
-    
-    for i in range(n):
-        if fix[i] and i in h:
-            h.pop(i)
-    return (h, J, fixed, offset)
-    
-
-
-# returns tuple (result before SPVAR, result after SPVAR)
-def test_spvar(h,
-               J,
-               sample_size,
-               fixing_threshold,
-               elite_threshold,
-               anneal_duration_one,
-               anneal_duration_many) -> tuple[float, float, int]:
-    (Q, _) = dimod.ising_to_qubo(h, J)
-
-    solution_old = generate_sample(Q, 1, anneal_duration_one).best
-
-    (spvar_h, spvar_J, fixed, _) = spvar(
-        h,
-        J,
-        sample_size,
-        fixing_threshold,
-        elite_threshold,
-        anneal_duration_many
+def from_honest_to_default(params: SPVAR_test_honest_params) -> SPVAR_default_params:
+    return SPVAR_default_params(
+        params.h.copy(),
+        params.J.copy(),
+        params.SPVAR_num_anneals,
+        params.fixing_threshold,
+        params.elite_threshold
     )
 
-    (spvar_Q, _) = dimod.ising_to_qubo(spvar_h, spvar_J)
-    solutionNew = generate_sample(spvar_Q, 1, anneal_duration_one).best
+class SPVAR:
 
-    state_new = solutionNew.state
+    def __init__(self):
+        pass
 
-    cnt_fixed = len(fixed.keys())
+    # returns new h, J, mapping dict and offset
+    def spvar(self, params : SPVAR_default_params) -> tuple[dict, dict, dict, int]:
+        
+        n = len(params.h.keys())
 
-    for v in fixed:
-        state_new[v] = 0 if fixed[v] == -1 else 1
+        samples = generate_sample(params.h, params.J, params.sample_size, params.anneal_duration)
+        samples.sort()
+        samples = samples[:int(params.sample_size * params.elite_threshold)]
 
-    before = solution_old.value
-    after = qubovert.utils.QUBOMatrix(Q).value(state_new)
-    return (before, after, cnt_fixed)
+        appears = np.array([np.array([sample.state[i] * 2 - 1 for sample in samples]) for i in range(n)])
+        deviation = ss.tstd(appears, axis=1)
+        avg = np.average(appears, axis=1)
 
+        fix = deviation < params.fixing_threshold + EPS
+        fixed = dict()
+
+        for i in range(n):
+            if (fix[i]):
+                fixed[i] = 1 if avg[i] > 0 else -1
+
+        offset = 0
+
+        J = params.J.copy()
+        h = params.h.copy()
+
+        for i in range(n):
+            if (fix[i]):
+                if ((i, i) in J):
+                    offset += J[(i, i)]
+                    J.pop((i, i))
+            
+            for j in range(i + 1, n):
+                if not fix[i] and not fix[j]:
+                    continue
+
+                sum_J = J.get((i, j), 0) + J.get((j, i), 0)
+
+                if fix[i] and fix[j]:
+                    offset += sum_J * fixed[i] * fixed[j]
+                    
+                if not fix[i]:
+                    h[i] += sum_J * fixed[j]
+                if not fix[j]:
+                    h[j] += sum_J * fixed[i]
+
+                if ((i, j) in J):
+                    J.pop((i, j))
+                if (j, i) in J:
+                    J.pop((j, i))
+        
+        for i in range(n):
+            if fix[i] and i in h:
+                h.pop(i)
+        return tuple([h, J, fixed, offset])
+        
+
+
+    # Compare result from annealer on given task with
+    # result from annealer on simplified by SPVAR task 
+    # Returns tuple (result before SPVAR, result after SPVAR)
+    def test_once(
+            self,
+            spvar_params: SPVAR_default_params,
+            anneal_duration_one
+        ) -> tuple[float, float, int]:
+
+        solution_old = generate_sample(spvar_params.h, spvar_params.J, 1, anneal_duration_one).best
+
+        (spvar_h, spvar_J, fixed, _) = self.spvar(spvar_params)
+
+        solutionNew = generate_sample(spvar_h, spvar_J, 1, anneal_duration_one).best
+
+        state_new = solutionNew.state
+
+        cnt_fixed = len(fixed.keys())
+
+        for v in fixed:
+            state_new[v] = 0 if fixed[v] == -1 else 1
+
+        before = solution_old.value
+        (Q_new, _) = dimod.ising_to_qubo(h, J)
+        after = qubovert.utils.QUBOMatrix(Q_new).value(state_new)
+        return (before, after, cnt_fixed)
+
+    # Comapares two solutions:
+    # 1) best of params.total_num_anneals results from annealer on given task
+    # 2) best of (params.total_num_anneals - params.SPVAR_num_anneals) results from annealer on simplified by SPVAR task
+    # In last case we spend params.SPVAR_num_anneals anneals to run SPVAR
+    # Returns tuple (result without spvar, result with spvar, count of fixed vars)
+    def test_honest(self, params : SPVAR_test_honest_params) -> tuple[int, int, int]:
+        solution_no_spvar = generate_sample(params.h, params.J, params.total_num_anneals).best
+        no_spvar_result = solution_no_spvar.value
+
+        default_params = from_honest_to_default(params)
+        [h_spvar, J_spvar, fixed, _] = self.spvar(default_params)
+
+        cnt_fixed = len(fixed.keys())
+
+        num_anneals_after_spvar = params.total_num_anneals - params.SPVAR_num_anneals
+        solution_spvar = generate_sample(h_spvar, J_spvar, num_anneals_after_spvar).best
+        state_spvar = solution_spvar.state
+
+        for v in fixed:
+            state_spvar[v] = 0 if fixed[v] == -1 else 1
+
+        # state_spvar = solution_spvar.state
+        (Q_spvar, _) = dimod.ising_to_qubo(params.h, params.J)
+        spvar_result = qubovert.utils.QUBOMatrix(Q_spvar).value(state_spvar)
+
+        return tuple([no_spvar_result, spvar_result, cnt_fixed])
--- test_spvar.py
+++ test_spvar.py
@@ -1,8 +1,15 @@
+from dataclasses import dataclass
 import spvar
 import io_maxcut
 import dimod
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from functools import *
+import os
+from math import *
 
+# Returns tuple[h, J, offset]
 def read_ising_from_Gset_file(path : str) -> tuple[dict, dict, int]:
     g = io_maxcut.read_gset_graph(path)
     Q_mat = io_maxcut.graph_to_qubo(g)
@@ -15,6 +22,7 @@ def read_ising_from_Gset_file(path : str) -> tuple[dict, dict, int]:
 
     return dimod.qubo_to_ising(Q_dict)
 
+#returns Q
 def read_qubo_from_csv_file(path : str) -> dict:
     f = open(path, "r")
     i = 0
@@ -26,168 +34,251 @@ def read_qubo_from_csv_file(path : str) -> dict:
         i += 1
     return Q
 
-# tests is a list of tuple[h, J, best_known]
-def test_criteria(sample_size,
-                  fixing_threshold,
-                  elite_threshold,
-                  anneal_duration_many,
-                  anneal_duration_one,
-                  tests,
-                  output_path,
-                  indexDataFrame,
-                  is_min = True,
-                  contains_best_known = True):
-    print("start testing criterea:")
-    print(f"sample_size: {sample_size}")
-    print(f"fixing_threshold: {fixing_threshold}")
-    print(f"elite_threshold: {elite_threshold}")
-    print(f"anneal_duration_one: {anneal_duration_one}")
-    print(f"anneal_duration_many: {anneal_duration_many}")
+#Returns list of tuples [h, J, offset]
+def read_qubo_matrices(dir_name : str) -> list[tuple[str, dict, dict]]:
+    testPathes = io_maxcut.files_in_directory(dir_name)
 
-    results = []
-    for test in tests:
-        h = dict()
-        J = dict()
-        best_known = None
-        if contains_best_known:
-            [h, J, best_known] = test
-        else:
-            [h, J] = test
-        
-        sz = len(h)
-
-        (before, after, cnt_fixed) = spvar.test_spvar(
-            h,
-            J,
-            sample_size,
-            fixing_threshold,
-            elite_threshold,
-            anneal_duration_one,
-            anneal_duration_many
-        )
-
-        if not is_min:
-            before = -before
-            after = -after
-
-        if contains_best_known:
-            results.append([
-                before,
-                after,
-                str(round(cnt_fixed / sz * 100), 1) + '%',
-                best_known,
-                str(round((before - best_known) / best_known * 100, 1)) + '%',
-                str(round((after - best_known) / best_known * 100, 1)) + '%',  
-                str(round((after - before) / before * 100, 1)) + '%',  
-            ])
-        else:
-            results.append([
-                before,
-                after,
-                str(round(cnt_fixed / sz * 100, 1)) + '%',
-                str(round((after - before) / before * 100, 1)) + '%',  
-            ])
-    
-    f = open(output_path, "a")
-    
-    f.write(f"sample_size: {sample_size}\n")
-    f.write(f"fixing_threshold: {fixing_threshold}\n")
-    f.write(f"elite_threshold: {elite_threshold}\n")
-    f.write(f"anneal_duration_one: {anneal_duration_one}\n")
-    f.write(f"anneal_duration_many: {anneal_duration_many}\n")
-    f.write(
-        pd.DataFrame(
-            data=results,
-            columns = [
-                "result before SPVAR",
-                "result after SPVAR",
-                "percent fixed varables",
-                "best known",
-                "(before - best) / best",
-                "(after - best) / best",
-                "(after - before) / before"
-            ] if contains_best_known
-            else [
-                "result before SPVAR",
-                "result after SPVAR",
-                "percent fixed varables",
-                "(after - before) / before"
-            ],
-            index = indexDataFrame
-            ).to_string() + "\n",
-    )
-    f.write("=" * (161 if contains_best_known else 105) + "\n")
-    f.close()
-
-
-def test_criteria_Gset(sample_size,
-                  fixing_threshold,
-                  elite_threshold,
-                  anneal_duration_many,
-                  anneal_duration_one) :
-
-    testPathes = io_maxcut.files_in_directory("Gset")
-
-    best_known_energy = {
-        'G1.dat': 11624,
-        'G3.dat': 11622,
-        'G7.dat': 2006,
-        'G11.dat': 564,
-        'G15.dat': 3050,
-        'G20.dat': 941,
-        'G25.dat': 13340,
-        'G28.dat': 3298,
-        'G33.dat': 1382,
-        'G40.dat': 2400,
-        'G43.dat': 6660,
-        'G44.dat': 6650
-    }
-
-    tests = []
+    result = []
     for testPath in testPathes:
-        path = "Gset/" + testPath
-        (h, J, _) = read_ising_from_Gset_file(path)
-        tests.append([h, J, best_known_energy[testPath]])
-    
-    test_criteria(sample_size,
-                  fixing_threshold,
-                  elite_threshold,
-                  anneal_duration_many,
-                  anneal_duration_one,
-                  tests,
-                  "output.txt",
-                  testPathes,
-                  False)
-
-def test_criteria_qubo_matrices(sample_size,
-                  fixing_threshold,
-                  elite_threshold,
-                  anneal_duration_many,
-                  anneal_duration_one):
-    testPathes = io_maxcut.files_in_directory("qubo_matrices")
-
-    tests = []
-    for testPath in testPathes:
-        path = "qubo_matrices/" + testPath
+        path = f"{dir_name}/" + testPath
         Q = read_qubo_from_csv_file(path)
         [h, J, _] = dimod.qubo_to_ising(Q)
-        tests.append([h, J])
+        result.append(tuple([testPath, h, J]))
     
-    test_criteria(sample_size,
-                  fixing_threshold,
-                  elite_threshold,
-                  anneal_duration_many,
-                  anneal_duration_one,
-                  tests,
-                  "output_new.txt",
-                  testPathes,
-                  contains_best_known=False)
+    return result
+
+@dataclass
+class Test_honest_params:
+    total_num_anneals : int
+    SPVAR_num_anneals : int
+    fixing_threshhold : float
+    elite_threshold : float
+
+# Does tests from params and returns results in list of DataFrames
+# with columns [Result without SPVAR, Result with SPVAR, % fixed vars]
+def test_honest(params : list[Test_honest_params], dir_tests : str) -> list[pd.DataFrame]:    
+    tests = read_qubo_matrices(dir_tests)
+    results = []
+    names = [name for [name, _, _] in tests]
+    s = spvar.SPVAR()
+    for param in params:
+        print(f"start test {param}")
+        test_results = []
+        for [_, h, J] in tests:
+            num_vars = len(h.keys())
+            spvar_params = spvar.SPVAR_honest_params(
+                h,
+                J,
+                param.total_num_anneals,
+                param.SPVAR_num_anneals,
+                param.fixing_threshhold,
+                param.elite_threshold
+            )
+            [no_spvar_result, spvar_result, cnt_fixed] = s.test_honest(spvar_params)
+            test_results.append([no_spvar_result, spvar_result, round(cnt_fixed / num_vars * 100, 1)])
+
+        columns = ["Result without SPVAR", "Result with SPVAR", "% fixed vars"]
+
+        results.append(pd.DataFrame(test_results, index = names, columns=columns))
+    
+    return results
+
+# Draws diagrams by results of honest tests and puts it
+# to file results.pdf in directory dir_results
+# typical case of using:
+#     test_results = test_honest(...)
+#     draw_bars_by_honest_results(..., test_results)
+#
+def draw_bars_by_honest_results(
+        total_num_anneals : int,
+        SPVAR_num_anneals_range : range,
+        fixing_threshhold : float,
+        elite_threshold : float,
+        dir_results : str,
+        results : list[pd.DataFrame]
+        ):
+    
+    if len(results) == 0:
+        print("No results:(")
+        return
+
+    test_names = results[0].index
+
+    sz = len(test_names)
+    
+    plt.rcParams.update({'font.size': 21})
+    fig, axs = plt.subplots(2, sz, figsize=(15 * sz, 25))
+
+    plt.suptitle(f"params:\n" +
+                 f"total_num_anneals = {total_num_anneals}\n" +
+                 f"fixing_threshhold = {fixing_threshhold}\n" +
+                 f"elite_threshold = {elite_threshold}")
+
+    for i in range (len(test_names)):
+        test_name = test_names[i]
+
+        columns = ["Result without SPVAR", "Result with SPVAR", "% fixed vars"]
+
+        data = {res : np.array([el.loc[test_name, res] for el in results]) for res in columns}
+        data["SPVAR number anneals"] = list(SPVAR_num_anneals_range)
+        frame = pd.DataFrame(data)
+
+        x = frame["SPVAR number anneals"]
+        y = (frame["Result with SPVAR"] - frame["Result without SPVAR"]) / np.abs(frame["Result without SPVAR"]) * 100
+
+        ax = axs[0] if sz == 1 else axs[0, i]
+
+        ax.bar(x, y, width=SPVAR_num_anneals_range.step / 5)
+        ax.set_ylabel("Growth of target function in %")
+        ax.set_xlabel("SPVAR number anneals")
+        ax.set_title(test_name, pad=30)
+
+        last = SPVAR_num_anneals_range.start
+        for el in SPVAR_num_anneals_range:
+            last = el
+
+        ax.hlines(
+            y = 0,
+            xmin=SPVAR_num_anneals_range.start,
+            xmax=last
+        )
+        
+        for container in ax.containers:
+            ax.bar_label(container, fmt='%.1f')
 
 
-# test_criteria_Gset(20, 0.1, 0.2, 20, 20)
+        ax = axs[1] if sz == 1 else axs[1, i]
 
-test_criteria_qubo_matrices(1000, 0.1, 0.2, 10000, 20)
-test_criteria_qubo_matrices(1000, 0.0, 0.2, 10000, 20)
-test_criteria_qubo_matrices(1000, 0.0, 0.1, 10000, 20)
-test_criteria_qubo_matrices(1000, 0.1, 0.1, 10000, 20)
+        x = frame["SPVAR number anneals"]
+        y = frame["% fixed vars"]
 
+        ax.bar(x, y, width=SPVAR_num_anneals_range.step / 5)
+        ax.set_ylabel("Percent of fixed vars")
+        ax.set_xlabel("SPVAR number anneals")
+
+        ax.hlines(
+            y = 0,
+            xmin=SPVAR_num_anneals_range.start,
+            xmax=last
+        )
+        
+        for container in ax.containers:
+            ax.bar_label(container, fmt='%.1f')        
+
+
+    plt.savefig(f"{dir_results}\\results.pdf")
+    plt.close()
+
+# Does some honest tests with params [total_num_anneals, SPVAR_num_anneals, fixing_threshhold, elite_threshold]
+# and saves results to files with names test_{SPVAR_num_anneals}.csv in dir_results directory
+# (SPVAR_num_anneals is an element of SPVAR_num_anneals_range);
+# then draw bar charts by results and saves in to the dir_results directory.
+# Also, if cached = True, this function gets results of tests from corresponding files in dir_results directory
+# (in this case results of tests have to be placed in dir_results directory)
+def test_different_num_anneals(
+        total_num_anneals : int,
+        SPVAR_num_anneals_range : range,
+        fixing_threshhold : float,
+        elite_threshold : float,
+        dir_results : str,
+        dir_data : str,
+        cached : bool = False
+        ):
+    try:
+        path = os.path.join(os.path.dirname(__file__), dir_results)
+        os.mkdir(path)
+    except OSError:
+        pass
+    
+    results = []
+    if cached:
+        results = [
+            pd.read_csv(f"{dir_results}\\test_{num}.csv", header=0, index_col=0)
+            for num in SPVAR_num_anneals_range
+        ]
+    else:
+        params = [
+            Test_honest_params(
+                total_num_anneals,
+                SPVAR_num_anneals,
+                fixing_threshhold,
+                elite_threshold
+            ) for SPVAR_num_anneals in SPVAR_num_anneals_range
+        ]
+
+        # test file exists
+        for i in range(len(params)):
+            fout = open(f"{dir_results}\\test_{params[i].SPVAR_num_anneals}.csv", 'a')
+            fout.close()
+        
+        results = test_honest(params, dir_data)
+
+        for i in range(len(params)):
+            fout = open(f"{dir_results}\\test_{params[i].SPVAR_num_anneals}.csv", 'w')
+            results[i].to_csv(fout)
+            fout.close()
+
+    draw_bars_by_honest_results(
+        total_num_anneals,
+        SPVAR_num_anneals_range,
+        fixing_threshhold,
+        elite_threshold,
+        dir_results,
+        results
+    )    
+
+def main():
+
+    data_dir = "test_data\\qubo_matrices"
+    results_dir = "test_results\\old"
+
+    test_different_num_anneals(200, range(100, 200, 10), 0.0, 0.2,
+                               f"{results_dir}\\test_200_0.0_0.2", data_dir, True)
+    
+    test_different_num_anneals(200, range(100, 200, 10), 0.1, 0.2,
+                               f"{results_dir}\\test_200_0.1_0.2", data_dir, True)
+    
+    test_different_num_anneals(1000, range(100, 1000, 100), 0.0, 0.1,
+                               f"{results_dir}\\test_1000_0.0_0.1", data_dir, True)
+    
+    test_different_num_anneals(1000, range(100, 1000, 100), 0.0, 0.2,
+                               f"{results_dir}\\test_1000_0.0_0.2", data_dir, True)
+    
+    test_different_num_anneals(1000, range(100, 1000, 100), 0.1, 0.1,
+                               f"{results_dir}\\test_1000_0.1_0.1", data_dir, True)
+
+    test_different_num_anneals(1000, range(100, 1000, 100), 0.1, 0.2,
+                               f"{results_dir}\\test_1000_0.1_0.2", data_dir, True)
+    
+    # log_arg_data = "test_data\\big_log_arc"
+    # biggest_data = "test_data\\biggest_log_arc"
+
+    # log_arg_results = "test_results\\log_arc"
+    
+    # biggest_results = "test_results\\biggest_log_arc"
+
+    # test_different_num_anneals(200, range(100, 200, 10), 0.0, 0.1,
+    #                            f"{log_arg_results}\\test_200_0.0_0.1", log_arg_data, True)
+
+    # test_different_num_anneals(500, range(100, 500, 50), 0.0, 0.1,
+    #                            f"{log_arg_results}\\test_500_0.0_0.1", log_arg_data, True)
+    
+    # test_different_num_anneals(1000, range(100, 1000, 100), 0.0, 0.1,
+    #                            f"{log_arg_results}\\test_1000_0.0_0.1", log_arg_data, True)
+
+    # test_different_num_anneals(500, range(100, 500, 50), 0.0, 0.2,
+    #                            f"{biggest_results}\\test_500_0.0_0.2", log_arg_data, True)
+
+    # test_different_num_anneals(500, range(100, 500, 50), 0.1, 0.1,
+    #                            f"{biggest_results}\\test_500_0.1_0.1", biggest_data, True)
+
+    # test_different_num_anneals(500, range(100, 500, 50), 0.1, 0.2,
+    #                            f"{biggest_results}\\test_500_0.1_0.2", biggest_data, True)
+    
+    # test_different_num_anneals(1000, range(100, 1000, 100), 0.1, 0.1,
+    #                            f"{biggest_results}\\test_1000_0.1_0.1", biggest_data, True)
+
+
+    
+
+main()
